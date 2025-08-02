c875a85757b631534a408eeafb59894c8133d582
Sakina Khan <sakinakhan6392@gmail.com>
2025-08-02 21:44:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.07%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)
+        return 1;
+        int a=1;
+        int b=1;
+        int c=a+b;
+        while(n>1)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            n--;
+        }
+        return c;
+    }
+};
