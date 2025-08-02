f21e73e7dd77d51e3e15bc456fb8d93b6eec2ad5
Sakina Khan <sakinakhan6392@gmail.com>
2025-08-02 21:24:27 +0530

    Time: 43 ms (86.36%), Space: 29 MB (72.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-     sort(nums.begin(),nums.end());
-     set<vector<int>> st;
-     map<int,int> m;
-     for(int i=0;i<nums.size();i++)
-     {
-        m[nums[i]]++;
-     }
-     int ans=0;
-     for(int i=0;i<nums.size();i++)
-     {
-        for(int j=i+1;j<nums.size();j++)
+        int n=nums.size();
+        vector<vector<int>> v;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
         {
-           int a=nums[i]+nums[j];
-           if(nums[j]==nums[j-1])
-           { if((nums[j]==0&&ans==1)||(nums[j]!=0))
+            if(i>0&&nums[i]==nums[i-1])
             continue;
-            else if(nums[j]==0)
+        int left=i+1;
+        int right=n-1;
+        while(left<right)
+        {
+            int val=nums[i]+nums[left]+nums[right];
+           
+             if(val>0)
             {
-                if(m[0]>2)
-                {
-                    st.insert({0,0,0});
-                    ans=1;
-                }
-                else ans=1;
-            }}
-           if(m[0-a])
-           {
-              if((-a==nums[i]&&m[-a]>1)||(-a==nums[j]&&m[-a]>1)||(-a!=nums[i]&&-a!=nums[j]))
-              {
-                if(a==0&&m[a]>2)
-                {
-                     vector<int> v={0,0,0};
-               st.insert(v);
-                }
-                else if(a!=0)
-                {
-                     vector<int> v={nums[i],nums[j],0-a};
-               sort(v.begin(),v.end());
-               st.insert(v);
-                }
-              }
-           }
+             right--;
+            }
+            else if(val<0)
+            left++;
+            else
+            {v.push_back({nums[i],nums[left],nums[right]});left++;
+            while(nums[left]==nums[left-1]&&left<right)
+            left++;
+            }
         }
-     }
-     vector<vector<int>> res;
-     for(auto p:st)
-     {
-        res.push_back(p);
-     }
-     return res;
+        }
+        return v;
     }
 };
