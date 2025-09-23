4b9698feff22ac86ac970bc6d95b84bd62055463
Sakina Khan <sakinakhan6392@gmail.com>
2025-09-23 15:04:29 +0530

    Time: 0 ms (100%), Space: 8.8 MB (10.4%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,58 +1,31 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        version1+=".";
-        version2+=".";
-       vector<int> v1,v2;
-       v1.push_back(-1);
-       v2.push_back(-1);
-       for(int i=0;i<version1.length();i++)
-       {
-        if(version1[i]=='.')
-        v1.push_back(i);
-       }
+        stringstream s1(version1);
+        stringstream s2(version2);
+        string t;
+        vector<int> v1,v2;
+        while(getline(s1,t,'.'))
+        {
+            v1.push_back(stoi(t));
+        }
+          while(getline(s2,t,'.'))
+        {
+            v2.push_back(stoi(t));
+        }
+        while(v1.size()<v2.size())
+        v1.push_back(0);
+        while(v2.size()<v1.size())
+        v2.push_back(0);
+        for(int i=0;i<v1.size();i++)
+        {
+            if(v1[i]<v2[i])
+            return -1;
+            if(v1[i]>v2[i])
+            return 1;
+        }
+        return 0;
 
-        for(int i=0;i<version2.length();i++)
-       {
-        if(version2[i]=='.')
-        v2.push_back(i);
-       }
-     int i=0,j=0;
-     while(i<v1.size()-1&&i<v2.size()-1)
-     {
-        string s1=version1.substr(v1[i]+1,v1[i+1]-v1[i]-1);
-        string s2=version2.substr(v2[i]+1,v2[i+1]-v2[i]-1);
-        int n1=stoi(s1);
-        int n2=stoi(s2);
-        if(n1<n2)
-        return -1;
-        if(n1>n2)
-        return 1;
-        i++;
-     }
-     while(i<v1.size()-1)
-     {
-        string s1=version1.substr(v1[i]+1,v1[i+1]-v1[i]-1);
-        int n1=stoi(s1);
-        int n2=0;
-         if(n1<n2)
-        return -1;
-        if(n1>n2)
-        return 1;
-        i++;
-     }
-      while(i<v2.size()-1)
-     {
-        string s2=version2.substr(v2[i]+1,v2[i+1]-v2[i]-1);
-        int n2=stoi(s2);
-        int n1=0;
-         if(n1<n2)
-        return -1;
-        if(n1>n2)
-        return 1;
-        i++;
-     }
-     return 0;
 
     }
 };
